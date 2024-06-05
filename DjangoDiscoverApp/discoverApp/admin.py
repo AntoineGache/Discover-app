3c65f3a2bcd9d4ba00f12d4e823cb0e7220a1953
antoinegache <antoinegache1@gmail.com>
2024-06-05 13:46:42 +0200

    Before presentation

--- DjangoDiscoverApp/discoverApp/admin.py
+++ DjangoDiscoverApp/discoverApp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Message, Abonnement
+from .models import Message, Abonnement, Commentaire, Like, Publication
 
 # Register your models here.
 
 admin.site.register(Message)
 admin.site.register(Abonnement)
+admin.site.register(Commentaire)
+admin.site.register(Like)
+admin.site.register(Publication)
